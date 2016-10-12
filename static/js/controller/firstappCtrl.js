008943df8b2c9805318896b371d19babc43861c6
ishwar17 <ishwardeoolkar171@gmail.com>
2016-10-12 12:41:48 +0530

    Form validation of signup but while submiiting form  is triggering..

--- static/js/controller/firstappCtrl.js
+++ static/js/controller/firstappCtrl.js
@@ -4,9 +4,12 @@ app.controller('firstappCtrl',['$scope',function ($scope){
     $scope.member = {};
     $scope.add = function(user){
         $scope.member = angular.copy(user);
+        $scope.signupForm.$setPristine();
         alert("User Data Infromation is Submitted....!!!!");
         console.log($scope.member);
+
         $scope.user ='';
+
     };
     /*$scope.compare = function (repass) {
         $scope.isconfirm = $scope.password == repass ? true : false;
